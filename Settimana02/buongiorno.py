0302775df300ba2aa95497f7052b64f5d2612aca
Fulvio Corno <fulvio.corno@polito.it>
2021-10-06 16:53:46 +0200

    esempi "sparsi" fatti a lezione

---
+++ Settimana02/buongiorno.py
@@ -0,0 +1,16 @@
+from math import sqrt
+
+print("Buongiorno")
+print("a tutti")
+
+print("Buongiorno", "a tutti")
+
+print(3, 2)
+print(3+2)
+
+a = sqrt(7)
+
+print("ABC", "XYZ")
+print("ABC"+"XYZ")
+# print("ABC" + 3)
+print("ABC" + str(3+2))
