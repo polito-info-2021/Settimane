93c91efafd977bd35ea95f898ac3ecf07a955666
Fulvio Corno <fulvio.corno@polito.it>
2022-01-13 13:37:06 +0100

    Commenti e documentazione funzioni

--- Settimana14/lab12_Hotel/hotel.py
+++ Settimana14/lab12_Hotel/hotel.py
@@ -2,6 +2,11 @@ from operator import itemgetter
 
 
 def leggi_presenze():
+    """
+    Acquisisci dal file presenze.txt i dati sugli ospiti dell'hotel,
+    memorizzandoli in una lista di dizionari
+    :return: lista di dizionari {nome, tel, ingresso, uscita}
+    """
     presenze = []
     with open('presenze.txt', 'r', encoding='utf-8') as file:
         for riga in file:
@@ -17,6 +22,10 @@ def leggi_presenze():
 
 
 def leggi_sospetti():
+    """
+    Acquisisce dal file sospetti.txt i nomi dei sospetti, costruendo una lista di stringhe
+    :return: lista di stringhe contenenti i nomi dei sospetti
+    """
     with open('sospetti.txt', 'r', encoding='utf-8') as file:
         sospetti = file.readlines()
     for i in range(len(sospetti)):
@@ -25,6 +34,14 @@ def leggi_sospetti():
 
 
 def cerca_contatti(ospite, presenze):
+    """
+    Determina se un ospite ha avuto dei contatti, controllando le sovrapposizioni
+    tra i giorni di permanenza in hotel
+    :param ospite: il nome (stringa) di un singolo ospite
+    :param presenze: l'elenco completo delle presenze
+    :return: se l'ospite non è in archivio: None. Se l'ospite non ha avuto contatti: [].
+    Se l'ospite ha avuto contatti: una lista di presenze.
+    """
     # cerca se l'ospite è stato presente
     ospite_trovato = None
     for presenza in presenze:
@@ -43,6 +60,11 @@ def cerca_contatti(ospite, presenze):
             ingresso_altro = presenza['ingresso']
             uscita_altro = presenza['uscita']
 
+            # verifico se 'presenza' è da considerarsi come un contatto
+            # condizione 1: 'presenza' non è lo stesso di 'ospite' (non devo controllare
+            # con me stesso
+            # condizione 2: le date dicono che c'è stata una sovrapposizione
+            # (vedi nota in fondo al file per la spiegazione)
             if (presenza['nome'] != ospite and
                     not (uscita_altro < ingresso_ospite or uscita_ospite < ingresso_altro)):
                 contatti.append(presenza)
@@ -69,6 +91,30 @@ def main():
 
 main()
 
+# DETERMINAZIONE DELL'AVVENUTA SOVRAPPOSIZIONE O MENO:
+# Indichiamo con [---] il periodo di permanenza di una persona:
+#
+# ospite          [--------]
+#
+# Le varie condizioni in cui i potrà trovare un 'altro', rispetto
+# a quelle dell'ospite (qui sopra) sono tutte e sole le 6 seguenti:
+#
+# altro1    [---]                              NO   uscita_altro < ingresso_ospite
+# altro2                       [----]          NO   ingresso_altro > uscita_ospite
+# altro3      [------]                         SI
+# altro4               [---------]             SI
+# altro5             [--]                      SI
+# altro6      [------------------]             SI
+#
+# Le condizioni con SI dicono che c'è stata una sovrapposizione, quelle con NO dicono
+# che non ci può essere stata.
+# Quindi:
+#  non sovrapposizione = uscita_altro < ingresso_ospite or ingresso_altro > uscita_ospite
+#  sovrapposizione = not( quello di sopra ^^ )
+
+
+# STRUTTURA DATI UTILIZZATA:
+# LISTA DI DIZIONARI
 #
 # [
 #     { 'nome': 'Anna Rossi', 'tel': '23432432', 'ingresso': 100, 'uscita': 105},
@@ -76,18 +122,10 @@ main()
 #     { 'nome': 'Anna Rossi', 'tel': '23432432', 'ingresso': 100, 'uscita': 105},
 #     { 'nome': 'Anna Rossi', 'tel': '23432432', 'ingresso': 100, 'uscita': 105},
 # ]
-#
+
+# ALTRA POSSIBILE STRUTTURA DATI (non utilizzata qui):
+# DIZIONARIO (chiave=nome) di DIZIONARI (record presenza)
 # {
 #     'Anna Rossi': {'tel': '23432432', 'ingresso': 100, 'uscita': 105},
 #     'Giuseppe Verdi': {'tel': '23432432', 'ingresso': 100, 'uscita': 105}
 # }
-
-
-# ospite          [--------]
-# altri    [---]                               NO   uscita_altro < ingresso_ospite
-#                              [----]          NO   ingresso_altro > uscita_ospite
-#             [------]                         SI
-#                      [---------]             SI
-#                    [--]                      SI
-#             [------------------]             SI
-
